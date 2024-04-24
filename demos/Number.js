8283fefdfd31c1b5f51db8a278b49315ed9ec507
Craig McKeachie <craigdmckeachie@gmail.com>
2024-04-24 15:26:47 -0400

    Number function

--- demos/Number.js
+++ demos/Number.js
@@ -2,7 +2,7 @@
 var hrsWorked = "33"; // a String object
 console.log(typeof hrsWorked);
 
-hrsWorked = parseInt(hrsWorked); // now a Number object
+hrsWorked = Number(hrsWorked); // now a Number object
 console.log(typeof hrsWorked);
 
 // output = doSomething(input1, input2);
