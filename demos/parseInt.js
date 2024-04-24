e09b1420fdab18abeabee44e5d2dc24c83e6195e
Craig McKeachie <craigdmckeachie@gmail.com>
2024-04-24 15:25:17 -0400

    parseInt

---
+++ demos/parseInt.js
@@ -0,0 +1,9 @@
+var hrsWorked;
+
+var hrsWorked = "33"; // a String object
+console.log(typeof hrsWorked);
+
+hrsWorked = parseInt(hrsWorked); // now a Number object
+console.log(typeof hrsWorked);
+
+// output = doSomething(input1, input2);
