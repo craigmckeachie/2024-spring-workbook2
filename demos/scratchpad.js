46e435f03f680d5cba85bad841d8f88885111458
Craig McKeachie <craigdmckeachie@gmail.com>
2024-04-24 14:05:46 -0400

    f to c

--- demos/scratchpad.js
+++ demos/scratchpad.js
@@ -2,8 +2,15 @@
 // console.log(favoriteBook);
 
 
-var favoriteBook; //declaration
+// var favoriteBook; //declaration
 
-favoriteBook = "Harry Potter: Prisoner of Azkaban"; //assignment
-console.log(favoriteBook);
+// favoriteBook = "Harry Potter: Prisoner of Azkaban"; //assignment
+// console.log(favoriteBook);
+
+
+var taxRate = 8.25;
+var taxMultiplier = .0825
+
+
+
 
---
+++ exercises/f_to_c.js
@@ -0,0 +1,15 @@
+"use strict";
+
+//define your inputs (variables)
+var fahrenheitTemperature = 77;
+
+//do the calculation C = (°F - 32) × 5/9
+var celsiusTempature = ((fahrenheitTemperature - 32) * 5) / 9;
+//print the results
+console.log(
+  "The fahrenheit temperature of " +
+    fahrenheitTemperature +
+    " is " +
+    celsiusTempature +
+    " celsius."
+);
