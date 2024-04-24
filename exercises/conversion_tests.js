7a931f80b658251c133c2d16aa9ee163ea79baf7
Craig McKeachie <craigdmckeachie@gmail.com>
2024-04-24 15:42:11 -0400

    conversion test 2

--- exercises/conversion_tests.js
+++ exercises/conversion_tests.js
@@ -1,12 +1,20 @@
 // Description: This script tests various numeric conversion techniques
 // Author: Craig McKeachie
 
-var a = " 101.1 ";
+var a = " 101.1 "; 
 var b = "55";
 var c = "402 Stevens";
 var d = "Number 5 ";
 
+// result = doSomething(input);
 aResult = parseInt(a);
 console.log(aResult);
 
-console.log(parseInt(a));
+bResult = parseInt(b);
+console.log(bResult);
+
+cResult = parseInt(c);
+console.log(cResult);
+
+dResult = parseInt(d);
+console.log(dResult);
