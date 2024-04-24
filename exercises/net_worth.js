a6cb121f877e1295c061a5e26c8d999fa9dcb1d8
Craig McKeachie <craigdmckeachie@gmail.com>
2024-04-24 10:59:38 -0400

    net worth

---
+++ exercises/net_worth.js
@@ -0,0 +1,35 @@
+"use strict";
+
+//lots of variables
+// var assets = 30000;
+// var debts = 2000;
+// var netWorth = assets - debts;
+// var message = "Your net worth is ";
+
+// console.log(message + netWorth);
+
+
+//less variables
+// var assets = 30000;
+// var debts = 2000;
+// var netWorth = assets - debts;
+
+// console.log("Your net worth is " + netWorth);
+
+//few variables
+// var assets = 30000;
+// var debts = 2000;
+
+// console.log("Your net worth is " + (assets - debts));
+
+
+
+// create boxes to hold:
+//assets
+var assets = 10000;
+//debts
+var debts = 2000;
+//calculate net worth and store in box
+var netWorth = assets - debts;
+//display message and networth
+console.log("Your net worth is " + netWorth);
