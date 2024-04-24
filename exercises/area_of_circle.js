5f799b0f304318c75979dc8edd7ce9dc578595e3
Craig McKeachie <craigdmckeachie@gmail.com>
2024-04-24 11:26:49 -0400

    area of circle

---
+++ exercises/area_of_circle.js
@@ -0,0 +1,8 @@
+// store radius
+var radius = 7;
+//calculate area of circle and store in area A = pi * r^2
+var PI = 3.1415;
+var area = PI * radius ** 2;
+//display "The area of a circle with radius ___ is ____";
+console.log(area);
+console.log("The area of a circle with radius " + radius + " is " + area);
