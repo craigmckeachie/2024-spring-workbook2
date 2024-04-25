11bf691f1931ba49656456a6ddc2cf74fabf2c81
Craig McKeachie <craigdmckeachie@gmail.com>
2024-04-25 15:40:15 -0400

    update

--- exercises/first_function.js
+++ exercises/first_function.js
@@ -1,8 +1,43 @@
-"use strict"
+"use strict";
 
 function greet() {
   let message = "Hello World!";
   console.log(message);
 }
 
-greet();
+
+function brushTeeth(){
+    console.log("Get up", "fds", "dfsk", "dfdsk");
+    console.log("Walk to the bathroom");
+    console.log("Open bathroom door");
+    console.log("Get up");
+    console.log("Walk to the bathroom");
+    console.log("Open bathroom door");
+    console.log("Get up");
+    console.log("Walk to the bathroom");
+    console.log("Open bathroom door");
+    console.log("Get up");
+    console.log("Walk to the bathroom");
+    console.log("Open bathroom door");
+    console.log("Get up");
+    console.log("Walk to the bathroom");
+    console.log("Open bathroom door");
+    console.log("Get up");
+    console.log("Walk to the bathroom");
+    console.log("Open bathroom door");
+    console.log("Get up");
+    console.log("Walk to the bathroom");
+    console.log("Open bathroom door");
+    console.log("Get up");
+    console.log("Walk to the bathroom");
+    console.log("Open bathroom door");
+    console.log("Get up");
+    console.log("Walk to the bathroom");
+    console.log("Open bathroom door");  
+    console.log("brushed done");
+}
+
+brushTeeth();
+brushTeeth();
+
+
