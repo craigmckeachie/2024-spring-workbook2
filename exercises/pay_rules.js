19153e670d947448f0c84269655dd369dde8726a
Craig McKeachie <craigdmckeachie@gmail.com>
2024-04-25 12:03:56 -0400

    update

--- exercises/pay_rules.js
+++ exercises/pay_rules.js
@@ -1,32 +1,24 @@
 // //declare variables
 
-var hoursWorked = 20;
-var regularPayRate = 12.5;
+// var hoursWorked = 20;
+// var regularPayRate = 12.5;
 
-// var hoursWorked = 40;
-// var regularPayRate = 25;
+var hoursWorked = 40
+var regularPayRate = 25;
 
 // var hoursWorked = 45;
 // var regularPayRate = 17.3;
 
 var regularHours = 40;
-var overtimePayRate = regularPayRate * 1.5;
 var overtimeHours = hoursWorked - regularHours;
-var regularPay;
-var overtimePay;
+var overtimePayRate = regularPayRate * 1.5;
+var overtimePay = overtimePayRate * overtimeHours;
 var grossPay;
 
-regularPay = regularPayRate * hoursWorked; //regularPay
-//make calculation for regular pay
-if (hoursWorked > regularHours) {
-  overtimePay = overtimePayRate * overtimeHours;
+if (hoursWorked <= 40) {
+  grossPay = regularPayRate * hoursWorked;
 } else {
-  //make calculation for overtime pay
-  
+  grossPay = regularPayRate * regularHours + overtimePay;
 }
 
-grossPay = regularPay + overtimePay;
-// calculate grossPay by adding regular + overtime
-
-//display result
 console.log(grossPay);
