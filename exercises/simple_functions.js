98a0e6fe88b9cc419f00e36a22692a5b7560050b
Craig McKeachie <craigdmckeachie@gmail.com>
2024-04-29 14:58:34 -0400

    simple functions

---
+++ exercises/simple_functions.js
@@ -0,0 +1,16 @@
+function favoriteThings() {
+  console.log("Hip-Hop Music");
+  console.log("Leo: My Dog");
+  console.log("My Kids and my Family");
+  console.log("Pistachios");
+}
+
+favoriteThings();
+
+
+function whyImHere() {
+    console.log("Because it is fun to have a positive impact on people's lives.");
+    console.log("Mohammed B is funny.");
+}
+
+whyImHere();
